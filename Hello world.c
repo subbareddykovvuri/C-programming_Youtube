59b47e299ffcb47d36e4e99e6c0ab052fd790d52
ManikantaSatyaSubbareddy <50864579+subbareddykovvuri@users.noreply.github.com>
2020-10-20 17:55:52 +0530

    Create Hello world.c

---
+++ Hello world.c
@@ -0,0 +1,8 @@
+#include<stdio.h>//includes the standard input and output to the program.
+int main(){
+	printf("Hello World");
+	return 0;
+}
+// to save the program press CTRL+S 
+//and enter the name you want to save as
+//follow me.
